b2c1bed5fb6d55f8bf9374ba34796b1bd3e6d1b7
abdu mk <mk22malika@gmail.com>
2023-05-14 12:26:18 -0700

    task all

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+__init__.py
+#!/usr/bin/python3
+"""Initializes the package"""
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+amenity.py
+#!/usr/bin/python3
+"""This module creates a Amenity class"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class for managing amenity objects"""
+
+    name = ""
